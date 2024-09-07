f38f3d2d83b13faa8f4e783e41a0eadcd7a75766
sk9712 <susk1297@gmail.com>
2024-09-07 15:52:18 +0530

    initial commit

---
+++ build.bat
@@ -0,0 +1,6 @@
+set MAVEN_HOME="D:\Projects\Project-LockBoxDB\Tools\apache-maven-3.9.9"
+
+set JAVA_HOME="D:\Projects\Project-LockBoxDB\Tools\openlogic-openjdk-21.0.4+7-windows-x64"
+
+cmd /k set PATH=%PATH%;%MAVEN_HOME%\bin;%JAVA_HOME%;
+
---
+++ src/main/java/com/lockboxdb/Main.java
@@ -0,0 +1,8 @@
+package com.lockboxdb;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
